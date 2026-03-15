ddc828e3544c558011f1e75e1e49d6d3fdf2a7b9
Sergey Mukhalov <smukhalov@gmail.com>
2026-03-15 14:44:21 +0300

    v5

--- test_version.cpp
+++ test_version.cpp
@@ -1,14 +1,16 @@
-#define BOOST_TEST_MODULE test_version
-
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
+#include <catch2/catch_test_macros.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
+unsigned int Factorial(unsigned int number)
 {
-    BOOST_CHECK(version() > 0);
+    return number <= 1 ? number : Factorial(number - 1) * number;
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+TEST_CASE("Factorials are computed", "[factorial]")
+{
+    REQUIRE(Factorial(1) == 1);
+    REQUIRE(Factorial(2) == 2);
+    REQUIRE(Factorial(3) == 6);
+    REQUIRE(Factorial(10) == 3628800);
+}
